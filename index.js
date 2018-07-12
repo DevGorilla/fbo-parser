8c27c2ad00b41dae7f0c80fb40842a5d7dbcd7c4
devgorilla <dwbrown89@gmail.com>
2018-07-11 21:47:08 -0700

    cleaner

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ module.exports = (function() {
       }
 
       return new SyntaxError(
-        message !== null ? message : buildMessage(expected, found) + `${posDetails.line}:${posDetails.column}`,
+        message !== null ? message : `${buildMessage(expected, found)} Position ${posDetails.line}:${posDetails.column}`,
         expected,
         found,
         pos,
